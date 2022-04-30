3c44d88457fd31ba701588f083a2aafc5c4e4555
Marlon Klagenberg <marlon.klc@gmail.com>
2022-04-30 16:05:04 -0300

    adjust text of challenge

--- Algorithms/inventory-update.js
+++ Algorithms/inventory-update.js
@@ -1,6 +1,9 @@
 /*
 # Inventory Update
-Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+Update the current existing inventory item quantities (in arr1). 
+If an item cannot be found, add the new item and quantity into the inventory array. 
+The returned inventory array should be in alphabetical order by item.
 */
 function updateInventory(arr1, arr2) {
     const newArray = !!arr1.length ? arr1 : arr2;
